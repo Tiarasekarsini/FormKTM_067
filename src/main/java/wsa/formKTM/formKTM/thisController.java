c434b474b639694b44a5f04283560967306629ac
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2022-11-13 12:37:02 +0700

    Mencoba menambahkan method dan model pada controller

--- src/main/java/wsa/formKTM/formKTM/thisController.java
+++ src/main/java/wsa/formKTM/formKTM/thisController.java
@@ -5,7 +5,14 @@
  */
 package wsa.formKTM.formKTM;
 
+import java.io.IOException;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 
 /**
@@ -14,5 +21,23 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class thisController {
+    @RequestMapping("/displaydata")
+    @ResponseBody
+    
+    public String dataMahasiswa(
+            @RequestParam("nMaha") String form1,
+            @RequestParam("nNIM") String form2,
+            @RequestParam("nPS") String form3,
+            @RequestParam("nFotoMaha") MultipartFile form4,
+            Model paket
+    )throws IOException{
+        String blob = Base64.encodeBase64String(form4.getBytes());
+        
+        paket.addAttribute("pMaha", form1);
+        paket.addAttribute("pNIM", form2);
+        paket.addAttribute("pPS", form3);
+        
+        return "view"+"<center><br/><br/><img src='data:image/*;base64,"+blob+"' /><br/><br/><center/>";
+    }
     
 }

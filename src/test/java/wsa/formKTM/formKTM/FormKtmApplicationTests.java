b60257678520f75c712bdcfb148610f15a6e4959
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2022-11-13 10:35:33 +0700

    ini commit pertama

---
+++ src/test/java/wsa/formKTM/formKTM/FormKtmApplicationTests.java
@@ -0,0 +1,13 @@
+package wsa.formKTM.formKTM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormKtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
